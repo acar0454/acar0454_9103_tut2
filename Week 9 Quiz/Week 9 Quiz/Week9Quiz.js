c9f1defe7e5119aa876ed522cd67876aedb165a2
“acar0454” <acar0454@uni.sydney.edu.au>
2023-10-26 13:33:55 +1100

    Updated var names

--- Week 9 Quiz/Week 9 Quiz/Week9Quiz.js
+++ Week 9 Quiz/Week 9 Quiz/Week9Quiz.js
@@ -1,54 +1,67 @@
 // Define the initial variables for the position and size of the brush:
-let brushX;
-let brushY;
+let rectX;
+let rectY;
+//let rectSize;
+//let easing = 0.05;
 
 function setup() {
-createCanvas(600, 600);
-background(255);
-frameRate(7);
-noStroke();
+  createCanvas(600, 600);
+  background(255);
+  frameRate(5);
+  noStroke();
 
-// Give the brush a starting position here by initialising the x and y variables:
-brushX = 0;
-brushY = 0;
+  // Give the brush a starting position:
+  rectX = 0;
+  rectY = 0;
 }
 
 function draw() {
-colorMode(HSB);
-fill(random(360), random(100), random(300), 255);
+  colorMode(HSBA);
+  fill(random(360), 70, 160, 80);
 
-// Translate the origin to the center of the canvas:
-translate(width/2, height/2);
+  // Give the brush a starting and ending position by initialising variables:
+  // let x = lerp(startX, endX, t);
+  // let y = lerp(startY, endY, t);
 
-// Define the number of sectors
-let sector = 70;
-// Use 'for' loops to create sets of patterns;
+  // Update interpolation factor with easing function:
+  // t += (1-t) * 0.05;
+
+  // Translate the origin to the center of the canvas:
+  translate(width / 2, height / 2);
+
+  // Define the number of sectors
+  let sector = 20;
+  // Use 'for' loops to create sets of patterns;
   for (let i = 0; i < sector; i++) {
-    rectMode(RADIUS);
-    let angle = 360/sector;
+    rectMode(CENTER);
+    let angle = 360 / sector;
     // Use rotate function here to rotate the sectors:
-    rotate(angle); 
-  brushPainting(brushX, brushY);
-
-  // Create another set of identical patterns in symmetry. Use scale function
-  // with negative parameter to invert the pattern symmetrically.
-  // We call scale() to the value of -1 on either of the two parameters, but
-  // not both parameters: scale (-1, -1) will not create a symmetrical pattern.
-  push();
-  scale(1, -1);
-  brushPainting(brushX, brushY);
-  pop();
+    rotate(angle);
+    rectPainting(rectX, rectY);
+  
+    // Create another set of identical patterns in symmetry. Use scale function
+    // with negative parameter to invert the pattern symmetrically.
+    // We call scale() to the value of -1 on either of the two parameters, but
+    // not both parameters: scale (-1, -1) will not create a symmetrical pattern.
+    push();
+    scale(-1, 1);
+    rectPainting(rectX, rectY);
+    pop();
   }
-// Increment the position of brush over time - add some randomness to the brush path
-brushX += random(-30, 30);
-brushY += 10;
+  // Increment the position of brush over time - add some randomness to the brush path:
+  // startX += random(20, 10);
+  // startY += 10;
+  // endX += random(20, 10);
+  // endY += 10;
+  rectX += random(-20, 20);
+  rectY += random(5);
 }
 
-function brushPainting(x, y) {
-// Use translate function to add some movement to the brush. Remember to use 'push' and 'pop' to 
-// start and restore the drawing state.
-push();
-translate(x, y);
-rect(0, 0, 30, 30);
-pop();
+function rectPainting(x, y) {
+  // Use translate function to add some movement to the brush. Remember to use 'push' and 'pop' to 
+  // start and restore the drawing state.
+  push();
+  translate(x, y);
+  rect(0, 0, 50, 50);
+  pop();
 }
