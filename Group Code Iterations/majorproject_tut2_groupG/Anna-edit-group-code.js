35009ccebc435cdf6a103c97b2726741841a6ca1
“acar0454” <acar0454@uni.sydney.edu.au>
2023-11-21 12:23:00 +1100

    Rename arrays and variables; create 'cars' array

--- Group Code Iterations/majorproject_tut2_groupG/Anna-edit-group-code.js
+++ Group Code Iterations/majorproject_tut2_groupG/Anna-edit-group-code.js
@@ -2,19 +2,19 @@
 // Creative Coding Major Project
 // Chosen artwork: Piet Mondrian's 'Broadway Boogie Woogie'
 
-// Create variables to hold values for buttons to play different music tracks on mouse press:
-let song1;
-let song2;
-let song3;
-let button1;
-let button2;
-let button3;
+// // Create variables to hold values for buttons to play different music tracks on mouse press:
+// let song1;
+// let song2;
+// let song3;
+// let button1;
+// let button2;
+// let button3;
 
-function preload() {
-  song1 = loadSound('audio/pixabay_Musictown_funky-dance-pop-1-95460.mp3');
-  song2 = loadSound('audio/pixabay_juliusH_big-apple-jazzy-piano-and-bass-music-13305.mp3');
-  song3 = loadSound('audio/pixabay_grandproject_lady-of-the-80x27s-128379.mp3');
-}
+// function preload() {
+//   song1 = loadSound('audio/pixabay_Musictown_funky-dance-pop-1-95460.mp3');
+//   song2 = loadSound('audio/pixabay_juliusH_big-apple-jazzy-piano-and-bass-music-13305.mp3');
+//   song3 = loadSound('audio/pixabay_grandproject_lady-of-the-80x27s-128379.mp3');
+// }
 
 // Create variables to hold values for creating rectangles:
 const numberOfAqua = 8;
@@ -23,269 +23,288 @@ let carSpacing = 10;
 const blueCars = ['#08527d', '#1678A2', '#093169'];
 const blueCars2 = ['#Bffbfd', '#083360', '#2a91a4'];
 
-// Create arrays to store multiple classes of rectangles:
-let rectangles = [];
+// Create arrays to store values for classes of rectangles - roads, cars and blocks:
+let roads = [];
 let cars = [];
-let rectanglesBig = [];
-let car1;
-let car2;
-let car3;
+let blocks = [];
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.position = (200, 200); // Position the canvas
 
-  car1 = new Car(100, 300, blueCars[0]);
-  car2 = new Car(0, 200, blueCars[1]);
-  car3 = new Car(200, 100, blueCars[2]);
+  // // Create buttons to play different music:
+  // button1 = createButton("Take me to funkytown");
+  // button1.position(10, 10);
+  // button1.mousePressed(toggleFunkyMusic);
 
-  console.log("car1", car1);
-  console.log("car2", car2);
-  console.log("car3", car3);
+  // function toggleFunkyMusic() {
+  //   if (song1.isPlaying()) {
+  //     song1.pause();
+  //     button1.html("Keep playing!");
+  //   } else {
+  //     song1.play();
+  //     song1.setVolume(0.3);
+  //     button1.html("Pause");
+  //   }
+  // }
 
-  car1.start(2.3);
-  car2.start(-4);
-  car3.start(random(-1, 1));
+  // button2 = createButton("I'm feeling jazzy");
+  // button2.position(230, 10);
+  // button2.mousePressed(toggleJazzyMusic);
 
-  // Create buttons to play different music:
-  button1 = createButton("Take me to funkytown");
-  button1.position(10, 10);
-  button1.mousePressed(toggleFunkyMusic);
+  // function toggleJazzyMusic() {
+  //   if (song2.isPlaying()) {
+  //     song2.pause();
+  //     button2.html("More jazz, please!");
+  //   } else {
+  //     song2.play();
+  //     song2.setVolume(0.3);
+  //     button2.html("Pause");
+  //   }
+  // }
 
-  function toggleFunkyMusic() {
-    if (song1.isPlaying()) {
-      song1.pause();
-      button1.html("Keep playing!");
-    } else {
-      song1.play();
-      song1.setVolume(0.3);
-      button1.html("Pause");
-    }
-  }
+  // button3 = createButton("I love the 80s!");
+  // button3.position(400, 10);
+  // button3.mousePressed(toggle1980Music);
 
-  button2 = createButton("I'm feeling jazzy");
-  button2.position(230, 10);
-  button2.mousePressed(toggleJazzyMusic);
-
-  function toggleJazzyMusic() {
-    if (song2.isPlaying()) {
-      song2.pause();
-      button2.html("More jazz, please!");
-    } else {
-      song2.play();
-      song2.setVolume(0.3);
-      button2.html("Pause");
-    }
-  }
-
-  button3 = createButton("I love the 80s!");
-  button3.position(400, 10);
-  button3.mousePressed(toggle1980Music);
-
-  function toggle1980Music() {
-    if (song3.isPlaying()) {
-      song3.pause();
-      button3.html("Play me again!");
-    } else {
-      song3.play();
-      song3.setVolume(0.3);
-      button3.html("Pause");
-    }
-  }
+  // function toggle1980Music() {
+  //   if (song3.isPlaying()) {
+  //     song3.pause();
+  //     button3.html("Play me again!");
+  //   } else {
+  //     song3.play();
+  //     song3.setVolume(0.3);
+  //     button3.html("Pause");
+  //   }
+  // }
 
   background('#000a0c');
 
-// The long, thin, aqua-coloured rectangles (or, 'roads') will appear at a random
-// y position. The little blue rectangles (or, 'cars') will draw on the lines, so
-// they have same y position as the roads do. 
+  //   // The long, thin, aqua-coloured rectangles (or, 'roads') will appear at a random
+  //   // y position. The little blue rectangles (or, 'cars') will draw on the lines, so
+  //   // they have same y position as the roads do. 
 
   let y1 = random(0, 150);
   let y2 = y1;
 
   // Use width and height instead of windowWidth and windowHeight to create 'roads':
 
-  // Upper four aqua 'roads' (long, thin rectangles):
+  // Upper four aqua 'roads' (long, thin rectangles): 
   for (let i = 0; i < 4; i++) {
     let aquaRoad = new Rectangle(0, i * y1, width, 15, '#Bffbfd');
-    rectangles.push(aquaRoad);
+    roads.push(aquaRoad);
   }
   // Lower four horizontal aqua 'roads':
   for (let i = 0; i < 4; i++) {
     let aquaRoad = new Rectangle(0, 250 + i * y1 * 2, width, 15, '#Bffbfd');
-    rectangles.push(aquaRoad);
+    roads.push(aquaRoad);
   }
 
   // Left three vertical aqua 'roads':
   for (let i = 0; i < 3; i++) {
     let aquaRoad = new Rectangle(i * y1, 0, 15, height, '#Bffbfd');
-    rectangles.push(aquaRoad);
+    roads.push(aquaRoad);
   }
   // Right three vertical aqua 'roads':
   for (let i = 0; i < 3; i++) {
     let aquaRoad = new Rectangle(250 + i * y1 * 2, 0, 15, height, '#Bffbfd');
-    rectangles.push(aquaRoad);
+    roads.push(aquaRoad);
   }
 
-  // Create small blocks positioned at random along the aqua roads:
-
-  // 1
   for (let i = 0; i < numberOfCars; i++) {
     const randomBlue = blueCars[i % blueCars.length];
-    let randomCar = new Rectangle(y2, i * (50 + carSpacing), 15, 15, randomBlue);
-    cars.push(randomCar);
-  }
-  // 2
-  for (let j = 0; j < 5; j++) {
-    for (let i = 0; i < numberOfCars; i++) {
-      const randomBlue = blueCars[j % blueCars.length];
-      let randomCar = new Rectangle(y2 + (1 * y1), j * (100 + carSpacing), 15, 15, randomBlue);
-      cars.push(randomCar);
-    }
+    
+    // Create a new Car instance and push it to the 'cars' array
+    const newCar = new Car(0, i * y1, randomBlue);
+    newCar.start(random(-2, 2)); // Set a random speed between -2 and 2
+    cars.push(newCar);
   }
 
-  // 3
-  for (let j = 0; j < 5; j++) {
-    for (let i = 0; i < numberOfCars; i++) {
-      const randomBlue = blueCars[i % blueCars.length];
-      let randomCar = new Rectangle(250 + y1 * 2 - y1 * 2, (i * (50 + carSpacing)), 15, 15, randomBlue);
-      cars.push(randomCar);
-    }
-  }
-  // 4
-  for (let i = 0; i < numberOfCars; i++) {
-    const randomBlue = blueCars[i % blueCars.length];
-    let randomCar = new Rectangle(250 + y1 * 2, i * (35 + carSpacing), 15, 15, randomBlue);
-    cars.push(randomCar);
-  }
-  // 5
-  for (let j = 0; j < 5; j++) {
-    for (let i = 0; i < numberOfCars; i++) {
-      const randomBlue = blueCars[i % blueCars.length];
-      let randomCar = new Rectangle(250 + y1 * 2 + y1 * 2, (i * (80 + carSpacing)), 15, 15, randomBlue);
-      cars.push(randomCar);
-    }
-  }
-
-  // 6
-  for (let j = 0; j < numberOfCars; j++) {
-    for (let i = 0; i < numberOfCars; i++) {
-      const randomBlue = blueCars[i % blueCars.length];
-      let randomCar = new Rectangle(i * (50 + carSpacing), y1 - y1, 15, 15, randomBlue);
-      cars.push(randomCar);
-    }
-  }
-  // 7
-  for (let i = 0; i < numberOfCars; i++) {
-    const randomBlue = blueCars[i % blueCars.length];
-    let randomCar = new Rectangle(i * (30 + carSpacing), y2, 15, 15, randomBlue);
-    cars.push(randomCar);
-  }
-  // 8
-  for (let j = 0; j < numberOfCars; j++) {
-    for (let i = 0; i < numberOfCars; i++) {
-      const randomBlue = blueCars[i % blueCars.length];
-      let randomCar = new Rectangle(i * (60 + carSpacing), y1 + y1, 15, 15, randomBlue);
-      cars.push(randomCar);
-    }
-  }
-  // 9
-  for (let i = 0; i < numberOfCars; i++) {
-    const randomBlue = blueCars[i % blueCars.length];
-    let randomCar = new Rectangle(i * (30 + carSpacing), y1 + y1 + y1, 15, 15, randomBlue);
-    cars.push(randomCar);
-  }
-
-  // 10
-  for (let i = 0; i < numberOfCars; i++) {
-    const randomBlue = blueCars[i % blueCars.length];
-    let randomCar = new Rectangle(i * (60 + carSpacing), 250, 15, 15, randomBlue);
-    cars.push(randomCar);
-  }
-  // 11
-  for (let i = 0; i < numberOfCars; i++) {
-    const randomBlue = blueCars[i % blueCars.length];
-    let randomCar = new Rectangle(i * (30 + carSpacing), 250 + y1 * 2, 15, 15, randomBlue);
-    cars.push(randomCar);
-  }
-  // 12
-  for (let i = 0; i < numberOfCars; i++) {
-    const randomBlue = blueCars[i % blueCars.length];
-    let randomCar = new Rectangle(i * (50 + carSpacing), 250 + y1 * 2 + y1 * 2, 15, 15, randomBlue);
-    cars.push(randomCar);
-  }
-  // 13
-  for (let i = 0; i < numberOfCars; i++) {
-    const randomBlue = blueCars[i % blueCars.length];
-    let randomCar = new Rectangle(i * (40 + carSpacing), 250 + y1 * 2 + y1 * 2 + y1 * 2, 15, 15, randomBlue);
-    cars.push(randomCar);
-  }
-
+  // Log the array of cars to the console for verification
+  console.log(cars);
   noStroke();
-
-  for (let i = 0; i < 15; i++) {
-
-    // Randomly generate the position, size and colour of block A:
-    let x = random(width);
-    let y = random(height);
-    let widthA = random(30, 50);
-    let heightA = random(30, 50);
-
-    // Randomly generate the size of blocks B and C:
-    let widthB = widthA - random(10, 20);
-    let heightB = heightA - random(10, 20);
-    let widthC = widthA + random(10, 20);
-    let heightC = heightA + random(10, 20);
-
-    // Randomly generate colours from blues2 array:
-
-    let carColourA = random(blueCars2);
-    let carColourB = random(blueCars2);
-    let carColourC = random(blueCars2);
-
-    // Create a new block object and add it to an array:
-
-    rectanglesBig.push(new Rectangle(x, y, widthC, heightC, carColourC));
-    rectanglesBig.push(new Rectangle(x, y, widthA, heightA, carColourA));
-    rectanglesBig.push(new Rectangle(x, y, widthB, heightB, carColourB));
-
-  }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background('#000a0c');
 
-car1.display();
-car2.display();
-car3.display();
+  // Display and move all cars in the array
+  // Draw roads, cars and blocks:
 
-car1.move();
-car2.move();
-car3.move();
+  for (let i = 0; i < roads.length; i++) {
+    roads[i].draw();
+  }
+  push();
+  rectMode(CORNER);
+  console.log(roads[i]);
 
-// Draw roads, cars and blocks (rectanglesBig):
-
-    for (let i = 0; i < rectangles.length; i++) {
-      rectangles[i].draw();
-    }
-    push();
-    rectMode(CENTER);
-
-    for (let i = 0; i < cars.length; i++) {
-      cars[i].draw();
-    }
-    push;
-
-    for(let i = 0; i < rectanglesBig.length; i++) {
-      rectanglesBig[i].draw();
-    }
-    pop();
-  
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].move();
+    console.log(cars[i]);
   }
 
+}
+
+
+
+
+//   // Create small 'cars' along the aqua roads: new Rectangle (x, y, width, height, color)
+
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Car(y2, i * (50 + carSpacing), randomBlue);
+//     cars.push(randomCar);
+//   }
+
+//   // 1
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Rectangle(y2, i * (50 + carSpacing), 15, 15, randomBlue);
+//     cars.push(randomCar);
+//   }
+//   2
+//   for (let j = 0; j < 5; j++) {
+//     for (let i = 0; i < numberOfCars; i++) {
+//       const randomBlue = blueCars[j % blueCars.length];
+//       let randomCar = new Rectangle(y2 + (1 * y1), j * (100 + carSpacing), 15, 15, randomBlue);
+//       cars.push(randomCar);
+//     }
+//   }
+
+//   // 3
+//   for (let j = 0; j < 5; j++) {
+//     for (let i = 0; i < numberOfCars; i++) {
+//       const randomBlue = blueCars[i % blueCars.length];
+//       let randomCar = new Rectangle(250 + y1 * 2 - y1 * 2, (i * (50 + carSpacing)), 15, 15, randomBlue);
+//       cars.push(randomCar);
+//     }
+//   }
+//   // 4
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Rectangle(250 + y1 * 2, i * (35 + carSpacing), 15, 15, randomBlue);
+//     cars.push(randomCar);
+//   }
+//   // 5
+//   for (let j = 0; j < 5; j++) {
+//     for (let i = 0; i < numberOfCars; i++) {
+//       const randomBlue = blueCars[i % blueCars.length];
+//       let randomCar = new Rectangle(250 + y1 * 2 + y1 * 2, (i * (80 + carSpacing)), 15, 15, randomBlue);
+//       cars.push(randomCar);
+//     }
+//   }
+
+//   // 6
+//   for (let j = 0; j < numberOfCars; j++) {
+//     for (let i = 0; i < numberOfCars; i++) {
+//       const randomBlue = blueCars[i % blueCars.length];
+//       let randomCar = new Rectangle(i * (50 + carSpacing), y1 - y1, 15, 15, randomBlue);
+//       cars.push(randomCar);
+//     }
+//   }
+//   // 7
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Rectangle(i * (30 + carSpacing), y2, 15, 15, randomBlue);
+//     cars.push(randomCar);
+//   }
+//   // 8
+//   for (let j = 0; j < numberOfCars; j++) {
+//     for (let i = 0; i < numberOfCars; i++) {
+//       const randomBlue = blueCars[i % blueCars.length];
+//       let randomCar = new Rectangle(i * (60 + carSpacing), y1 + y1, 15, 15, randomBlue);
+//       cars.push(randomCar);
+//     }
+//   }
+//   // 9
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Rectangle(i * (30 + carSpacing), y1 + y1 + y1, 15, 15, randomBlue);
+//     cars.push(randomCar);
+//   }
+
+//   // 10
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Rectangle(i * (60 + carSpacing), 250, 15, 15, randomBlue);
+//     cars.push(randomCar);
+//   }
+//   // 11
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Rectangle(i * (30 + carSpacing), 250 + y1 * 2, 15, 15, randomBlue);
+//     cars.push(randomCar);
+//   }
+//   // 12
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Rectangle(i * (50 + carSpacing), 250 + y1 * 2 + y1 * 2, 15, 15, randomBlue);
+//     cars.push(randomCar);
+//   }
+//   // 13
+//   for (let i = 0; i < numberOfCars; i++) {
+//     const randomBlue = blueCars[i % blueCars.length];
+//     let randomCar = new Rectangle(i * (40 + carSpacing), 250 + y1 * 2 + y1 * 2 + y1 * 2, 15, 15, randomBlue);
+//     cars.push(randomCar);
+//   }
+
+//   noStroke();
+
+//   for (let i = 0; i < 15; i++) {
+
+//     // Randomly generate the position, size and colour of block A:
+//     let x = random(width);
+//     let y = random(height);
+//     let widthA = random(30, 50);
+//     let heightA = random(30, 50);
+
+//     // Randomly generate the size of blocks B and C:
+//     let widthB = widthA - random(10, 20);
+//     let heightB = heightA - random(10, 20);
+//     let widthC = widthA + random(10, 20);
+//     let heightC = heightA + random(10, 20);
+
+//     // Randomly generate colours from blues2 array:
+
+//     let carColourA = random(blueCars2);
+//     let carColourB = random(blueCars2);
+//     let carColourC = random(blueCars2);
+
+//     // Create a new block object and add it to an array:
+
+//     blocks.push(new Rectangle(x, y, widthC, heightC, carColourC));
+//     blocks.push(new Rectangle(x, y, widthA, heightA, carColourA));
+//     blocks.push(new Rectangle(x, y, widthB, heightB, carColourB));
+//   }
+// }
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+// function draw() {
+//   background('#000a0c');
+
+//   // Draw roads, cars and blocks (blocks):
+
+//   for (let i = 0; i < roads.length; i++) {
+//     roads[i].draw();
+//   }
+//   push();
+//   rectMode(CORNER);
+
+//   for (let i = 0; i < cars.length; i++) {
+//     cars[i].draw();
+//   }
+//   push();
+//   rectMode(CENTER);
+
+//   for (let i = 0; i < blocks.length; i++) {
+//     blocks[i].draw();
+//   }
+//   pop();
+
+// }
 
 class Rectangle {
   constructor(x, y, width, height, color) {
@@ -302,29 +321,29 @@ class Rectangle {
   }
 }
 
-class Car { 
+class Car {
   constructor(x, y, carColour) {
     this.x = x;
     this.y = y;
     this.color = carColour;
     this.speed = 0;
-}
-start(speed) {
- this.speed = speed;
-}
-display() {
-  fill(this.color);
-  rect(this.x, this.y, 20, 15);
-}
-move() {
-  this.x += this.speed;
-  // Wrap x around boundaries:
-  if (this.x < -20) {
-    this.x = width;
-  } else if (this.x > width) {
-    this.x = -20;
   }
-}
+  start(speed) {
+    this.speed = speed;
+  }
+  display() {
+    fill(this.color);
+    rect(this.x, this.y, 20, 15);
+  }
+  move() {
+    this.x += this.speed;
+    // Wrap x value around boundaries of the canvas 'width':
+    if (this.x < -20) {
+      this.x = width;
+    } else if (this.x > width) {
+      this.x = -20;
+    }
+  }
 }
 
 
@@ -361,3 +380,7 @@ and the rest refer to cars. Do you need separate variables names/classes?
 * Changed 'colorA', 'colorB' and 'colorC' to 'carColourA', 'carColourB' and 'carColourC' respectively.
 * Commented out //1 - //13 so small blocks (i.e. static 'cars') disappear in Live Server preview.
 */
+
+
+
+
